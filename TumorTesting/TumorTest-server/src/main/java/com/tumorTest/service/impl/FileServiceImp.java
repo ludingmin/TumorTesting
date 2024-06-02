4dec60fd86d5bc8b5ba7ddf7f21fd79c10d4e4de
zzk <zzk@gpnu.cn>
2024-06-02 14:44:09 +0800

    文件上传接口修改

--- TumorTesting/TumorTest-server/src/main/java/com/tumorTest/service/impl/FileServiceImp.java
+++ TumorTesting/TumorTest-server/src/main/java/com/tumorTest/service/impl/FileServiceImp.java
@@ -3,8 +3,10 @@ package com.tumorTest.service.impl;
 import com.tumorTest.context.BaseContext;
 import com.tumorTest.dto.UserDto;
 import com.tumorTest.excption.FileException;
+import com.tumorTest.mapper.BookingMapper;
 import com.tumorTest.result.Result;
 import com.tumorTest.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +22,9 @@ public class FileServiceImp implements FileService {
     @Value("${tumortest.basePath}")
     private String basePath;
 
+    @Autowired
+    private BookingMapper bookingMapper;
+
     /**
      * 文件上传方法
      * @param file 上传的文件对象
@@ -29,11 +34,21 @@ public class FileServiceImp implements FileService {
     public Result fileReload(MultipartFile file) {
         //        获取文件名
         String originalFilename = file.getOriginalFilename();
-//        获取后缀
-        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+//        获取最后的后缀
+        String lastsuffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //去掉最后一个后最，在获取倒数第二个后缀
+        String removeSuffix = originalFilename.substring(0,originalFilename.lastIndexOf("."));
+        System.out.println("removeSuffix:"+removeSuffix);
+        String suffix = removeSuffix.substring(removeSuffix.lastIndexOf(".")) + lastsuffix;
+
+        System.out.println("后缀："+suffix);
+
+        UserDto user = BaseContext.getUser();
+        Long bookingId = bookingMapper.selectBookingIdIntegerByUserId(user.getId());
 
 //        使用uuid生成文件名字，防止名字重复导致的文件覆盖
-        String fileName = UUID.randomUUID().toString() + suffix;
+//        String fileName = UUID.randomUUID().toString() + suffix;
+        String fileName = bookingId + suffix;
 
 //        创建目录
         File dir = new File(basePath);
